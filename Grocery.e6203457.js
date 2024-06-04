7dadc0e37ce5a7a8ba374de4ed63cf33322613f5
shlok008 <shlokaggarwal008@gmail.com>
2024-06-04 18:48:44 +0530

    Updates

---
+++ Grocery.e6203457.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire4b21;(0,e.register)("loR3J",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>n,set:void 0,enumerable:!0,configurable:!0});var l=e("ayMG0"),n=()=>(0,l.jsx)("h1",{children:"Our online Grocery is under development, see you soon."})});
+//# sourceMappingURL=Grocery.e6203457.js.map
